47ed41e7d44ea865a52eb42106b25f304573358f
nyxSeal <litigate_putdown.zigzagged008@slmails.com>
2026-01-27 15:21:04 -0500

    fixed syntax error

--- configuration.nix
+++ configuration.nix
@@ -45,10 +45,10 @@
      };
      
      kernelParams = {
-       "zswap.enabled=1" # enables zswap
-       "zswap.compressor=lz4" # compression algorithm
-       "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
-       "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+       "zswap.enabled=1"; # enables zswap
+       "zswap.compressor=lz4"; # compression algorithm
+       "zswap.max_pool_percent=20"; # maximum percentage of RAM that zswap is allowed to use
+       "zswap.shrinker_enabled=1"; # whether to shrink the pool proactively on high memory pressure
      };
      
      initrd.systemd.enable = true; # required if using lz4 algorithm for zswap
