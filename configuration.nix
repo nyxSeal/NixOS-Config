fa61d1efaea5511581e6c87e963c56881fd024b2
nyxSeal <litigate_putdown.zigzagged008@slmails.com>
2026-01-27 20:34:16 -0500

    lots of formatting, some comments added and some removed

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,63 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 { config, lib, pkgs, ... }:
 
+
+
 {
+
+
+
+
+
+# /$$$$$$ /$$      /$$ /$$$$$$$   /$$$$$$  /$$$$$$$  /$$$$$$$$ /$$$$$$ 
+#|_  $$_/| $$$    /$$$| $$__  $$ /$$__  $$| $$__  $$|__  $$__//$$__  $$
+#  | $$  | $$$$  /$$$$| $$  \ $$| $$  \ $$| $$  \ $$   | $$  | $$  \__/
+#  | $$  | $$ $$/$$ $$| $$$$$$$/| $$  | $$| $$$$$$$/   | $$  |  $$$$$$ 
+#  | $$  | $$  $$$| $$| $$____/ | $$  | $$| $$__  $$   | $$   \____  $$
+#  | $$  | $$\  $ | $$| $$      | $$  | $$| $$  \ $$   | $$   /$$  \ $$
+# /$$$$$$| $$ \/  | $$| $$      |  $$$$$$/| $$  | $$   | $$  |  $$$$$$/
+#|______/|__/     |__/|__/       \______/ |__/  |__/   |__/   \______/ 
+
+
+
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
+
+
+
+
+# /$$      /$$ /$$$$$$  /$$$$$$   /$$$$$$ 
+#| $$$    /$$$|_  $$_/ /$$__  $$ /$$__  $$
+#| $$$$  /$$$$  | $$  | $$  \__/| $$  \__/
+#| $$ $$/$$ $$  | $$  |  $$$$$$ | $$      
+#| $$  $$$| $$  | $$   \____  $$| $$      
+#| $$\  $ | $$  | $$   /$$  \ $$| $$    $$
+#| $$ \/  | $$ /$$$$$$|  $$$$$$/|  $$$$$$/
+#|__/     |__/|______/ \______/  \______/ 
+
+
+
+  documentation.dev.enable = true; # Enables extra man pages
+
+
+ 
+  nixpkgs.config.allowUnfree = true; # Allows unfree software to be installed
+
+
+
+
+
+#  /$$$$$$  /$$$$$$$  /$$$$$$$  /$$       /$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$$$ /$$$$$$  /$$$$$$  /$$   /$$  /$$$$$$ 
+# /$$__  $$| $$__  $$| $$__  $$| $$      |_  $$_/ /$$__  $$ /$$__  $$|__  $$__/|_  $$_/ /$$__  $$| $$$ | $$ /$$__  $$
+#| $$  \ $$| $$  \ $$| $$  \ $$| $$        | $$  | $$  \__/| $$  \ $$   | $$     | $$  | $$  \ $$| $$$$| $$| $$  \__/
+#| $$$$$$$$| $$$$$$$/| $$$$$$$/| $$        | $$  | $$      | $$$$$$$$   | $$     | $$  | $$  | $$| $$ $$ $$|  $$$$$$ 
+#| $$__  $$| $$____/ | $$____/ | $$        | $$  | $$      | $$__  $$   | $$     | $$  | $$  | $$| $$  $$$$ \____  $$
+#| $$  | $$| $$      | $$      | $$        | $$  | $$    $$| $$  | $$   | $$     | $$  | $$  | $$| $$\  $$$ /$$  \ $$
+#| $$  | $$| $$      | $$      | $$$$$$$$ /$$$$$$|  $$$$$$/| $$  | $$   | $$    /$$$$$$|  $$$$$$/| $$ \  $$|  $$$$$$/
+#|__/  |__/|__/      |__/      |________/|______/ \______/ |__/  |__/   |__/   |______/ \______/ |__/  \__/ \______/ 
 
 
 
@@ -19,10 +67,10 @@
     borgbackup
     btop
     fastfetch
-    floorp-bin
+    floorp-bin # replacement for librewolf
     kdePackages.ksshaskpass
     kdePackages.filelight
-    #librewolf
+    #librewolf  # currently is broken
     prismlauncher
     tealdeer
     vesktop
@@ -31,94 +79,9 @@
     vorta
   ];
 
-  
 
 
-
-  # Use the systemd-boot EFI boot loader.
-  boot = {
-     
-    loader = {
-       systemd-boot.enable = true;
-       efi.canTouchEfiVariables = true;
-       timeout = 10;
-     };
-     
-     kernelParams = [
-       "zswap.enabled=1" # enables zswap
-       "zswap.compressor=lz4" # compression algorithm
-       "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
-       "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
-     ];
-     
-     initrd.systemd.enable = true; # required if using lz4 algorithm for zswap     
-     kernelPackages = pkgs.linuxPackages_hardened; # Use latest (hardened) kernel
-  };
-
-  services.swapspace.enable = true; # dynamically creates swap when needed on SSD
-
-
-
-
-  networking = {
-    hostName = "nixy"; # Define your hostname.
-    networkmanager.enable = true; # Enables network configurations interactively with nmcli or nmtui
-  };
-
-
-
-
-  time.timeZone = "America/New_York"; # Timezone
-
-
-
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-
-
-
-  system.autoUpgrade = { # Automatic updating
-    enable = true;
-    dates = "weekly";
-  };
-
-  nix = { 
-
-    gc = { # Automatic rollback version cleanup
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 10d";
-    };
-
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [ "nix-command" "flakes" ];
-    };
-
-  };
-
-
-
-
-  programs.git = {
-    enable = true;
-
-    config = {
-      user.name = "nyxSeal";
-      user.email = "litigate_putdown.zigzagged008@slmails.com";
-      pull.rebase = false;
-      #commit.gpgsign = true;
-    };
-
-  };
-
-
-
-
-  programs.bash = {
+  programs.bash = { # shell language
     enable = true;
 
     shellAliases = { # Bash aliases
@@ -132,8 +95,77 @@
 
 
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
-  services.openssh = { # Enables OpenSSH
+
+
+
+
+#/$$$$$$$   /$$$$$$  /$$$$$$$  /$$$$$$$$ /$$$$$$ /$$$$$$$$ /$$$$$$  /$$$$$$  /$$   /$$ /$$$$$$ /$$   /$$  /$$$$$$ 
+#| $$__  $$ /$$__  $$| $$__  $$|__  $$__/|_  $$_/|__  $$__/|_  $$_/ /$$__  $$| $$$ | $$|_  $$_/| $$$ | $$ /$$__  $$
+#| $$  \ $$| $$  \ $$| $$  \ $$   | $$     | $$     | $$     | $$  | $$  \ $$| $$$$| $$  | $$  | $$$$| $$| $$  \__/
+#| $$$$$$$/| $$$$$$$$| $$$$$$$/   | $$     | $$     | $$     | $$  | $$  | $$| $$ $$ $$  | $$  | $$ $$ $$| $$ /$$$$
+#| $$____/ | $$__  $$| $$__  $$   | $$     | $$     | $$     | $$  | $$  | $$| $$  $$$$  | $$  | $$  $$$$| $$|_  $$
+#| $$      | $$  | $$| $$  \ $$   | $$     | $$     | $$     | $$  | $$  | $$| $$\  $$$  | $$  | $$\  $$$| $$  \ $$
+#| $$      | $$  | $$| $$  | $$   | $$    /$$$$$$   | $$    /$$$$$$|  $$$$$$/| $$ \  $$ /$$$$$$| $$ \  $$|  $$$$$$/
+#|__/      |__/  |__/|__/  |__/   |__/   |______/   |__/   |______/ \______/ |__/  \__/|______/|__/  \__/ \______/                   
+
+
+  boot = {
+     
+    loader = {
+       systemd-boot.enable = true;
+       efi.canTouchEfiVariables = true;
+       timeout = 10; # extends the amount of time the generation table on boot is shown
+     };
+     
+     kernelParams = [
+       "zswap.enabled=1" # enables zswap
+       "zswap.compressor=lz4" # compression algorithm
+       "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+       "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+     ];
+     
+     initrd.systemd.enable = true; # required if using lz4 algorithm for zswap     
+     kernelPackages = pkgs.linuxPackages_hardened; # use latest (hardened) kernel
+  };
+
+
+
+  services.swapspace.enable = true; # dynamically creates swap when needed on SSD
+
+
+
+
+
+# /$$   /$$ /$$$$$$$$ /$$$$$$$$ /$$      /$$  /$$$$$$  /$$$$$$$  /$$   /$$ /$$$$$$ /$$   /$$  /$$$$$$ 
+#| $$$ | $$| $$_____/|__  $$__/| $$  /$ | $$ /$$__  $$| $$__  $$| $$  /$$/|_  $$_/| $$$ | $$ /$$__  $$
+#| $$$$| $$| $$         | $$   | $$ /$$$| $$| $$  \ $$| $$  \ $$| $$ /$$/   | $$  | $$$$| $$| $$  \__/
+#| $$ $$ $$| $$$$$      | $$   | $$/$$ $$ $$| $$  | $$| $$$$$$$/| $$$$$/    | $$  | $$ $$ $$| $$ /$$$$
+#| $$  $$$$| $$__/      | $$   | $$$$_  $$$$| $$  | $$| $$__  $$| $$  $$    | $$  | $$  $$$$| $$|_  $$
+#| $$\  $$$| $$         | $$   | $$$/ \  $$$| $$  | $$| $$  \ $$| $$\  $$   | $$  | $$\  $$$| $$  \ $$
+#| $$ \  $$| $$$$$$$$   | $$   | $$/   \  $$|  $$$$$$/| $$  | $$| $$ \  $$ /$$$$$$| $$ \  $$|  $$$$$$/
+#|__/  \__/|________/   |__/   |__/     \__/ \______/ |__/  |__/|__/  \__/|______/|__/  \__/ \______/ 
+
+
+
+  networking = {
+    hostName = "nixy";
+    networkmanager.enable = true; # enables network configurations interactively with nmcli or nmtui
+  };
+
+
+
+  time.timeZone = "America/New_York";
+
+
+
+  services.openssh = {
     enable = true;
     ports = [ 5000 22 ];
 
@@ -143,6 +175,9 @@
     };
 
   };
+
+
+
   programs.ssh = {
     startAgent = true;
     enableAskPassword = true;
@@ -164,22 +199,9 @@
 
 
 
-
   environment.variables = {
     SSH_ASKPASS_REQUIRE = "prefer";
   };
-
-
-
-
-  security = {
-    unprivilegedUsernsClone = true; # Allows apps to be launched with hardened kernel
-    apparmor.enable = true;
-    #allowUserNamespaces = true; # Needed for sandboxing?
-    sudo.execWheelOnly = true;
-    protectKernelImage = true;
-  };  
-
 
 
 
@@ -197,27 +219,127 @@
 
 
 
-
-  documentation.dev.enable = true; # Enables extra man pages
-
-
-
- 
-  nixpkgs.config.allowUnfree = true; # Allows unfree software to be installed
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
 
 
- 
-  programs.steam = { # Enables steam
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+
+
+
+
+# /$$      /$$  /$$$$$$  /$$$$$$ /$$   /$$ /$$$$$$$$ /$$$$$$$$ /$$   /$$  /$$$$$$  /$$   /$$  /$$$$$$  /$$$$$$$$
+#| $$$    /$$$ /$$__  $$|_  $$_/| $$$ | $$|__  $$__/| $$_____/| $$$ | $$ /$$__  $$| $$$ | $$ /$$__  $$| $$_____/
+#| $$$$  /$$$$| $$  \ $$  | $$  | $$$$| $$   | $$   | $$      | $$$$| $$| $$  \ $$| $$$$| $$| $$  \__/| $$      
+#| $$ $$/$$ $$| $$$$$$$$  | $$  | $$ $$ $$   | $$   | $$$$$   | $$ $$ $$| $$$$$$$$| $$ $$ $$| $$      | $$$$$   
+#| $$  $$$| $$| $$__  $$  | $$  | $$  $$$$   | $$   | $$__/   | $$  $$$$| $$__  $$| $$  $$$$| $$      | $$__/   
+#| $$\  $ | $$| $$  | $$  | $$  | $$\  $$$   | $$   | $$      | $$\  $$$| $$  | $$| $$\  $$$| $$    $$| $$      
+#| $$ \/  | $$| $$  | $$ /$$$$$$| $$ \  $$   | $$   | $$$$$$$$| $$ \  $$| $$  | $$| $$ \  $$|  $$$$$$/| $$$$$$$$
+#|__/     |__/|__/  |__/|______/|__/  \__/   |__/   |________/|__/  \__/|__/  |__/|__/  \__/ \______/ |________/
+
+
+
+  system.autoUpgrade = { # Automatic updating
     enable = true;
-    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    dates = "weekly";
   };
 
 
 
-  
+  nix = { 
+
+    gc = { # Automatic rollback version cleanup
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 10d";
+    };
+
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+
+  };
+
+
+
+
+
+# /$$$$$$$   /$$$$$$   /$$$$$$  /$$   /$$ /$$   /$$ /$$$$$$$ 
+#| $$__  $$ /$$__  $$ /$$__  $$| $$  /$$/| $$  | $$| $$__  $$
+#| $$  \ $$| $$  \ $$| $$  \__/| $$ /$$/ | $$  | $$| $$  \ $$
+#| $$$$$$$ | $$$$$$$$| $$      | $$$$$/  | $$  | $$| $$$$$$$/
+#| $$__  $$| $$__  $$| $$      | $$  $$  | $$  | $$| $$____/ 
+#| $$  \ $$| $$  | $$| $$    $$| $$\  $$ | $$  | $$| $$      
+#| $$$$$$$/| $$  | $$|  $$$$$$/| $$ \  $$|  $$$$$$/| $$      
+#|_______/ |__/  |__/ \______/ |__/  \__/ \______/ |__/      
+
+
+
+  programs.git = {
+    enable = true;
+
+    config = {
+      user.name = "nyxSeal";
+      user.email = "litigate_putdown.zigzagged008@slmails.com";
+      pull.rebase = false;
+      #commit.gpgsign = true;
+    };
+
+  };
+
+
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+
+
+
+
+#  /$$$$$$  /$$$$$$$$  /$$$$$$  /$$   /$$ /$$$$$$$  /$$$$$$ /$$$$$$$$ /$$     /$$
+# /$$__  $$| $$_____/ /$$__  $$| $$  | $$| $$__  $$|_  $$_/|__  $$__/|  $$   /$$/
+#| $$  \__/| $$      | $$  \__/| $$  | $$| $$  \ $$  | $$     | $$    \  $$ /$$/ 
+#|  $$$$$$ | $$$$$   | $$      | $$  | $$| $$$$$$$/  | $$     | $$     \  $$$$/  
+# \____  $$| $$__/   | $$      | $$  | $$| $$__  $$  | $$     | $$      \  $$/   
+# /$$  \ $$| $$      | $$    $$| $$  | $$| $$  \ $$  | $$     | $$       | $$    
+#|  $$$$$$/| $$$$$$$$|  $$$$$$/|  $$$$$$/| $$  | $$ /$$$$$$   | $$       | $$    
+# \______/ |________/ \______/  \______/ |__/  |__/|______/   |__/       |__/    
+
+
+
+  security = {
+    unprivilegedUsernsClone = true; # Allows apps to be launched with hardened kernel
+    apparmor.enable = true;
+    #allowUserNamespaces = true; # Needed for sandboxing?
+    sudo.execWheelOnly = true;
+    protectKernelImage = true;
+  };  
+
+
+
+
+
+# /$$   /$$  /$$$$$$  /$$$$$$$$ /$$$$$$$   /$$$$$$ 
+#| $$  | $$ /$$__  $$| $$_____/| $$__  $$ /$$__  $$
+#| $$  | $$| $$  \__/| $$      | $$  \ $$| $$  \__/
+#| $$  | $$|  $$$$$$ | $$$$$   | $$$$$$$/|  $$$$$$ 
+#| $$  | $$ \____  $$| $$__/   | $$__  $$ \____  $$
+#| $$  | $$ /$$  \ $$| $$      | $$  \ $$ /$$  \ $$
+#|  $$$$$$/|  $$$$$$/| $$$$$$$$| $$  | $$|  $$$$$$/
+# \______/  \______/ |________/|__/  |__/ \______/ 
+
+
+
   users.users.nyxSeal = { # define a user account
     isNormalUser = true;
     extraGroups = [ "wheel" ".git" ]; # Enable ‘sudo’ for the user.
@@ -240,20 +362,28 @@
   #   enableSSHSupport = true;
   # };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+
+
+
+# /$$$$$$$   /$$$$$$        /$$   /$$  /$$$$$$  /$$$$$$$$       /$$$$$$$$ /$$$$$$  /$$   /$$  /$$$$$$  /$$   /$$
+#| $$__  $$ /$$__  $$      | $$$ | $$ /$$__  $$|__  $$__/      |__  $$__//$$__  $$| $$  | $$ /$$__  $$| $$  | $$
+#| $$  \ $$| $$  \ $$      | $$$$| $$| $$  \ $$   | $$            | $$  | $$  \ $$| $$  | $$| $$  \__/| $$  | $$
+#| $$  | $$| $$  | $$      | $$ $$ $$| $$  | $$   | $$            | $$  | $$  | $$| $$  | $$| $$      | $$$$$$$$
+#| $$  | $$| $$  | $$      | $$  $$$$| $$  | $$   | $$            | $$  | $$  | $$| $$  | $$| $$      | $$__  $$
+#| $$  | $$| $$  | $$      | $$\  $$$| $$  | $$   | $$            | $$  | $$  | $$| $$  | $$| $$    $$| $$  | $$
+#| $$$$$$$/|  $$$$$$/      | $$ \  $$|  $$$$$$/   | $$            | $$  |  $$$$$$/|  $$$$$$/|  $$$$$$/| $$  | $$
+#|_______/  \______/       |__/  \__/ \______/    |__/            |__/   \______/  \______/  \______/ |__/  |__/
+
+
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  system.stateVersion = "25.11"; # DO NOT CHANGE
+  system.stateVersion = "25.11"; # did you read?
+
+
+
+
 
 }
 
