2794a7fe4e4f2415171c118987675b1165bad767
nyxSeal <litigate_putdown.zigzagged008@slmails.com>
2026-01-29 19:29:10 -0500

    reverted previous commit, hopefully fixed the actual error

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs, global }:
+  outputs = { self, nixpkgs, global } @inputs:
   let
   pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    nixosConfigurations.nixy = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.${global.hostName} = nixpkgs.lib.nixosSystem {
 
       specialArgs = { 
         inherit inputs;
