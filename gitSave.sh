397443e5c23a2d35fae6a92f21493feefe057d2c
nyxSeal <litigate_putdown.zigzagged008@slmails.com>
2026-02-16 22:30:43 -0500

    fix error (now rebase after commiting changes)

--- gitSave.sh
+++ gitSave.sh
@@ -2,7 +2,6 @@
 
 cd ~/.nixconfig
 
-git pull --rebase
 
 git add -v ~/.nixconfig/*
 git add -v ~/.nixconfig/.gitignore
@@ -10,6 +9,8 @@ git add -v ~/.nixconfig/.gitignore
 echo -n "Commit message: "
 read -r commitMessage
 
-git commit -m "$commitMessage" 
+git commit -m "$commitMessage"
+
+git pull --rebase
 
 git push --set-upstream origin main
