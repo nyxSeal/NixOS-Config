7e3d59f6357a4ff0fb92a2895f95b30e0d13acc2
nyxSeal <litigate_putdown.zigzagged008@slmails.com>
2026-02-16 22:29:34 -0500

    add home-manager

--- flake.nix
+++ flake.nix
@@ -4,27 +4,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    # if adding another input: make sure to add 'inputs.nixpkgs.follows = "nixpkgs";'
-    # this sacrifices a bit of reproducibility for not having to download another branch
-    # you can change "nixpkgs" at the end for any of the inputs (for example, if you had an input named 'unstable' that linked to the unstable branch, you could link an input to the unstable branch instead of stable)
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
 
 
-  outputs = { self, nixpkgs } @inputs:
+  outputs = { self, nixpkgs, home-manager, ...  } @inputs:
   let
   pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+  # CHANGE THESE VALUES TO APPLY SYSTEMWIDE
+  hostName = "nixy";
+  mainUser = "nyxSeal";
+  gitUsername = "nyxSeal";
+  gitEmail = "litigate_putdown.zigzagged008@slmails.com";
+  
   in {
-    nixosConfigurations.nixy = nixpkgs.lib.nixosSystem { # replace nixy for hostName if different
+
+    nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem { # replace nixy for hostName if different
 
       specialArgs = { 
         inherit inputs;
 
         global = {
-          hostName = "nixy";
-          mainUser = "nyxSeal";
-          gitUsername = "nyxSeal";
-          gitEmail = "litigate_putdown.zigzagged008@slmails.com";
+          hostName = ${hostName};
+          mainUser = ${mainUser};
+          gitUsername = ${gitUsername};
+          gitEmail = ${gitEmail};
         };
 
       };
@@ -33,6 +42,18 @@
         ./configuration.nix
       ];
 
-    };    
+    };
+
+    homeConfigurations.${mainUser} = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [
+        ./home.nix
+      ];
+    };
+
   };
+
+
+
 }
